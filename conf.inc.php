ba2d281bcf1da86e20843856bd7ff6ba2362b429
Oliver Schmidt <os.germany@icloud.com>
2015-11-03 20:22:03 +0100

    Create conf.inc.php
    user configuration outside of widget*.php-file, easier to update main code w/o touching the configuration

---
+++ conf.inc.php
@@ -0,0 +1,22 @@
+<?php
+/* * ****************************************
+ * **************** CONFIG v1 ***************
+ * **************************************** */
+
+// Edit the dbConnect to match your DB Log settings of fhem
+// For SQLite define the path to your db in your filesystem
+
+// DB Type, use 'sqlite' or 'mysql'
+$dbType = 'mysql';
+
+//SQLite
+//$dbPath = '/opt/fhem/fhem.db';
+
+// MySQL
+$host = 'localhost';
+$mysql_username = '';
+$mysql_password = '';
+$database = 'fhem';
+$port = 3306;
+
+?>
