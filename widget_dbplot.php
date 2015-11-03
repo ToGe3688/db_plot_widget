b0f5622f9eebd64c93166506d7c9791c2dfd0786
Tobias Geier <toge3688@gmail.com>
2015-11-03 20:34:37 +0100

    Move setAttribute after PDO Object creation

--- widget_dbplot.php
+++ widget_dbplot.php
@@ -79,8 +79,8 @@ if (!is_array($requestedDeviceReadings) && count($requestedDeviceReadings) == 0)
 if ($dbType == 'sqlite') {
     $db = new PDO('sqlite:' . $dbPath);
 } elseif ($dbType == 'mysql') {
-    $db->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
     $db = new PDO('mysql:host=' . $host . '; port='. $port .';dbname=' . $database, $mysql_username, $mysql_password);
+    $db->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
 }
 
 
